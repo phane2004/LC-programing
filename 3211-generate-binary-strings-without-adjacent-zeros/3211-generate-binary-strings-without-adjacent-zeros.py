d3e716a73e741e7f3c0c1ccfdba72ba6b03068e0
PHANI <dereddyphane@gmail.com>
2026-09-18 22:07:46 +0530

    Time: 57 ms (82.06%), Space: 20.4 MB (75.57%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
@@ -1,22 +1,15 @@
 class Solution:
-    def helper(self, s, n, uniq):
-        
+    def helper(self, s, n, res):
         if n == 0:
-            if len(s) < 2:
-                uniq.add(s)
-                
-            elif len(s) >= 2:
-                if any((s[i] == '0' and s[i - 1] == '0' for i in range(1, len(s)))):
-                    return
-                else:
-                    uniq.add(s)
-            #print(uniq)
+            res.append(s)
             return
 
-        self.helper(s + '0', n - 1, uniq)
-        self.helper(s + '1', n - 1, uniq)
+        self.helper(s + '1', n - 1, res)
+
+        if not s or s[-1] != '0':
+            self.helper(s + '0', n - 1, res)
+
     def validStrings(self, n: int) -> List[str]:
-        uniq = set()
-        self.helper("", n, uniq)
-        return list(uniq)
-        
+        res = []
+        self.helper("", n, res)
+        return res
